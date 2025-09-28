4ba4d0e26f09552a4ecd667ecadd0e8c9bb2af1c
ehmicky <ehmicky@gmail.com>
2025-09-28 12:00:18 +0100

    Use latest version of `handle-cli-error`

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
-import handleCliError, { validateOptions, type Options } from 'handle-cli-error'
+import beautifulError from 'beautiful-error'
+import handleCliError, {
+  validateOptions,
+  type Options as HandleCliErrorOptions,
+} from 'handle-cli-error'
 import type { Info, Plugin } from 'modern-errors'
 
 /**
  * Options of `modern-errors-cli`
  */
-export type { Options }
+export type Options = Omit<HandleCliErrorOptions, 'classes' | 'custom'>
+
+// `error.pretty()` is called with `errorString` as argument by
+// `beautiful-error`. We ignore that argument and prevent it from being
+// considered an invalid options object.
+const isOptions = (options: unknown) => typeof options !== 'string'
 
 const getOptions = (options: Options = {}) => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   validateOptions(options)
 
   if (options.classes !== undefined) {
-    throw new TypeError('"classes" must not be defined.')
+    throw new TypeError('The "classes" option must not be defined.')
+  }
+
+  if (options.custom !== undefined) {
+    throw new TypeError('The "custom" option must not be defined.')
   }
 
   return options
@@ -38,7 +50,30 @@ const getOptions = (options: Options = {}) => {
  * ```
  */
 const exit = ({ error, options }: Info<Options>['instanceMethods']) => {
-  handleCliError(error, options)
+  handleCliError(error, { ...options, custom: 'pretty' })
+}
+
+// Uses `.pretty()` to avoid conflict with `modern-errors-beautiful`.
+// Temporarily unsets `error.pretty()` to avoid recursion.
+const pretty = ({
+  error,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  options: { exitCode, silent, timeout, log, ...beautifulErrorOptions },
+}: Info<Options>['instanceMethods']) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(error, 'pretty', {
+    value: undefined,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  })
+
+  try {
+    return beautifulError(error, beautifulErrorOptions)
+  } finally {
+    // eslint-disable-next-line fp/no-delete
+    delete (error as Error & { pretty?: undefined }).pretty
+  }
 }
 
 /**
@@ -46,8 +81,9 @@ const exit = ({ error, options }: Info<Options>['instanceMethods']) => {
  */
 const modernErrorsCli = {
   name: 'cli' as const,
+  isOptions,
   getOptions,
-  instanceMethods: { exit },
+  instanceMethods: { exit, pretty },
 } satisfies Plugin
 
 export default modernErrorsCli
